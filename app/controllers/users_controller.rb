7d06e82773d0849818e2c855728742f0ee06609d
Fahim-at-work <shariff.fahim7@gmail.com>
2021-10-07 15:36:35 -0400

    added user actions and views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,21 @@
 class UsersController < ApplicationController
+  
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
+
+  def index
+    @users = User.all
+  end
+  
+
   def new
     @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
   end
 
   def create
@@ -13,7 +28,23 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = 'Your details were updated successfully'
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+  
+
   private
+
+  def set_users
+    @user = User.find(params[:id])
+  end
+  
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
