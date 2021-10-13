d5a528b6b5b74ef5439f80b6354c6a603b3b75fc
Fahim-at-work <shariff.fahim7@gmail.com>
2021-10-13 15:46:04 -0400

    added restriction on view and controller level using helpers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :require_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update]
   
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
@@ -15,12 +17,12 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome #{@user.username}!!"
       redirect_to articles_path
     else
@@ -29,7 +31,6 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = 'Your details were updated successfully'
       redirect_to articles_path
@@ -41,7 +42,7 @@ class UsersController < ApplicationController
 
   private
 
-  def set_users
+  def set_user
     @user = User.find(params[:id])
   end
   
@@ -50,5 +51,11 @@ class UsersController < ApplicationController
     params.require(:user).permit(:username, :email, :password)
   end
   
+  def require_same_user
+    if current_user != @user
+      flash[:alert] = 'You can only edit or delete your own profile'
+      redirect_to @user
+    end
+  end
   
 end
