9b7ccc97519b79d5a792bd008a327fe09bb24df1
Fahim-at-work <shariff.fahim7@gmail.com>
2021-11-10 15:25:29 -0500

    added fuctional and integration testing using tdd

---
+++ test/integration/create_category_test.rb
@@ -0,0 +1,33 @@
+require "test_helper"
+
+class CreateCategoryTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @admin_user = User.create(username: 'johndoe', email: 'john.doe@yopmail.com',
+                              password: 'password', admin: true)
+    sign_in_as(@admin_user)
+  end
+  
+  test "get new category form and create category" do
+    get '/categories/new'
+    assert_response :success
+    assert_difference 'Category.count', 1 do
+      post categories_url, params: { category: { name: 'Sports' } }
+      assert_response :redirect
+    end
+    follow_redirect!
+    assert_response :success
+    assert_match "Sports", response.body
+  end
+
+  test "get new category form and reject invalid submission" do
+    get '/categories/new'
+    assert_response :success
+    assert_no_difference "Category.count", 1 do
+      post categories_url, params: { category: { name: '' } }
+    end
+    assert_match "errors", response.body
+    assert_select 'h1.checking-errors'
+  end
+
+end
