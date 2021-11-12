6848036f22198b659590da5f1fbc020afcc2a336
Fahim-at-work <shariff.fahim7@gmail.com>
2021-11-12 15:49:49 -0500

    added categories actions

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -15,6 +15,20 @@ class CategoriesController < ApplicationController
       render 'new'
     end
   end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:notice] = 'Category was updated successfully'
+      redirect_to @category
+    else
+      render 'edit'
+    end
+  end
   
   def index
     @categories = Category.paginate(page: params[:page], per_page: 5)
@@ -22,7 +36,10 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
+    @articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
+
+
 
   private
 
@@ -32,7 +49,7 @@ class CategoriesController < ApplicationController
 
   def require_admin
     if !(logged_in? && current_user.admin?)
-      flash[:alert] = "Only Admins can create categories"
+      flash[:alert] = "Only Admins can create or update categories"
       redirect_to categories_path
     end
   end
